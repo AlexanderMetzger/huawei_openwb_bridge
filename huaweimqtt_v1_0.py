bf80d6d0d511deaa35d3c03a88b197738398a1c6
AlexanderMetzger <60460634+AlexanderMetzger@users.noreply.github.com>
2023-09-27 19:18:13 +0200

    Add files via upload

---
+++ huaweimqtt_v1_0.py
@@ -0,0 +1,227 @@
+# version 1.3 vom 20.08.23
+import time
+import asyncio
+import paho.mqtt.publish as publish
+import numpy as np
+import json
+import configparser
+from huawei_solar import HuaweiSolarBridge, register_names as rn, register_values as rv
+
+loop = asyncio.new_event_loop()
+
+def read_mqtt_host():
+    config = configparser.ConfigParser()
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    return config['MQTT']['host']
+
+# Function to read the counter / PV / BAT number from the INI file
+def read_counter_number():
+    config = configparser.ConfigParser()
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    return config.getint('counternumber', 'value')
+
+def read_bat_number():
+    config = configparser.ConfigParser()
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    return config.getint('batnumber', 'value')
+
+def read_pv_number():
+    config = configparser.ConfigParser()
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    return config.getint('pvnumber', 'value')
+
+
+# openWB IP:
+mqttclient = "PVImporter"
+
+# Dictionary to map register names to MQTT topics
+topic_mapping = {
+    'input_power': f"openWB/set/pv/{read_pv_number()}/get/power",
+    'storage_state_of_capacity': f"openWB/set/bat/{read_bat_number()}/get/soc",
+    'grid_accumulated_energy': f"openWB/set/counter/{read_counter_number()}/get/imported",
+    'grid_exported_energy': f"openWB/set/counter/{read_counter_number()}/get/exported",
+    'storage_charge_discharge_power': f"openWB/set/bat/{read_bat_number()}/get/power",
+    'accumulated_yield_energy': f"openWB/set/pv/{read_pv_number()}/get/exported",
+    #'daily_yield_energy': f"openWB/set/pv/{read_pv_number()}/get/exported",
+    'storage_current_day_charge_capacity': f"openWB/set/bat/{read_bat_number()}/get/imported",
+    'storage_current_day_discharge_capacity': f"openWB/set/bat/{read_bat_number()}/get/exported",
+    'power_meter_active_power': f"openWB/set/counter/{read_counter_number()}/get/power",
+    'grid_frequency': f"openWB/set/counter/{read_counter_number()}/get/frequency",
+
+}
+
+
+
+def publish2openWB(mqtttopic, message):
+    publish.single(mqtttopic, payload=message, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
+
+async def huaweiReadValues(topic_mapping):
+    bridge = await HuaweiSolarBridge.create(host="192.168.200.1", port=6607)
+
+    # Register names
+    registers_evu = ['grid_frequency', 'power_meter_active_power', 'active_grid_A_power',
+    'active_grid_B_power', 'active_grid_C_power', 'grid_A_voltage', 'grid_B_voltage',
+    'grid_C_voltage', 'active_grid_A_current', 'active_grid_B_current', 'active_grid_C_current',
+    'grid_exported_energy', 'grid_accumulated_energy', 'active_power', 'grid_voltage']
+    registers_wr = ['input_power', 'accumulated_yield_energy', 'active_power', 'daily_yield_energy']
+    registers_bat = ['storage_state_of_capacity', 'storage_charge_discharge_power', 'storage_current_day_charge_capacity', 'storage_current_day_discharge_capacity']
+    registers = registers_evu + registers_wr + registers_bat
+
+    # Main Tasks:
+    while True:
+        try:
+            if not bridge:
+                bridge = await HuaweiSolarBridge.create(host="192.168.200.1", port=6607)
+
+            # Dictionary to store the register values
+            register_values = {}
+
+            # Initialize an empty NumPy array for grid currents
+            array_currents_grid = np.array([])
+
+            # Initialize an empty NumPy array for grid voltages
+            array_voltages_grid = np.array([])
+            # Initialize an empty NumPy array for grid voltages
+            array_powers_grid = np.array([])
+            # Initialize an empty NumPy array for grid voltages
+            array_powerfactors_grid = np.array([])
+
+            for i in registers:
+                try:
+                    result = await bridge.client.get(i)
+
+                    if result[0] is not None and result[0] != 0:
+                        mqtttopic = False
+
+                        if str(i) == "input_power":
+                            mqtttopic = f"openWB/set/pv/{read_pv_number()}/get/power"
+                            message = result[0] * -1
+
+                        elif str(i) == "storage_state_of_capacity":
+                            mqtttopic = f"openWB/set/bat/{read_bat_number()}/get/soc"
+                            message = result[0]
+
+                        elif str(i) == "grid_accumulated_energy":
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/imported"
+                            message = result[0]
+
+                        elif str(i) == "grid_exported_energy":
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/exported"
+                            message = result[0]
+
+                        elif str(i) == "storage_charge_discharge_power":
+                            mqtttopic = f"openWB/set/bat/{read_bat_number()}/get/power"
+                            message = result[0]
+
+                        elif str(i) == "daily_yield_energy":
+                            mqtttopic = f"openWB/set/pv/{read_pv_number()}/get/exported"
+                            message = result[0] * 1000
+
+                        elif str(i) == "storage_current_day_charge_capacity":
+                            mqtttopic = f"openWB/set/bat/{read_bat_number()}/get/imported"
+                            message = result[0] * 1000
+
+                        elif str(i) == "storage_current_day_discharge_capacity":
+                            mqtttopic = f"openWB/set/bat/{read_bat_number()}/get/exported"
+                            message = result[0] * 1000
+
+                        elif str(i) == "power_meter_active_power":
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/power"
+                            message = result[0]  * -1
+ #                       elif str(i) == "power_factor":
+ #                           mqtttopic = "openWB/set/counter/16/get/power_factors"
+ #                           message = result[0]
+
+                        elif str(i) == "grid_frequency":
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/frequency"
+                            message = result[0]
+#                       elif str(i) == "grid_voltage":
+#                           mqtttopic = "openWB/counter/16/get/voltages"
+#                           message = result[0]
+                        # Handle other registers (in this case, the grid currents)
+                        # Append numerical grid currents to the array
+                        elif i in ['active_grid_A_current', 'active_grid_B_current', 'active_grid_C_current']:
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/currents"
+                            array_currents_grid = np.append(array_currents_grid, result[0] * -1)
+
+                        # Handle other registers (in this case, the grid voltages)
+                        # Append numerical grid voltages to the array
+                        elif i in ['grid_A_voltage', 'grid_B_voltage', 'grid_C_voltage']:
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/voltages"
+                            array_voltages_grid = np.append(array_voltages_grid, result[0])
+                        # Append numerical grid voltages to the array
+                        elif i in ['active_grid_A_power', 'active_grid_B_power', 'active_grid_C_power']:
+                            mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/powers"
+                            array_powers_grid = np.append(array_powers_grid, result[0] * -1)
+#                        elif i in ['active_grid_power_factor', 'active_grid_power_factor', 'active_grid_power_factor']:
+#                            mqtttopic = "openWB/set/counter/16/get/power_factors"
+#                            array_powerfactors_grid = np.append(array_powerfactors_grid, result[0])
+                        if mqtttopic is not False:
+                            register_values[i] = message
+
+                except:
+                    await bridge.stop()
+                    bridge = False
+                    pass
+
+            # Publish the values to their respective topics
+            for register, value in register_values.items():
+                mqtttopic = topic_mapping.get(register)
+                if mqtttopic:
+                    try:
+                        if register == "active_grid_currents":
+                            # Publish the current values as an array
+                            publish.single(mqtttopic, payload=value, qos=0, retain=False, hostname=mqtthost,
+                                           client_id=mqttclient)
+                        if register == "active_grid_voltages":
+                            # Publish the voltage values as an array
+                            publish.single(mqtttopic, payload=value, qos=0, retain=False, hostname=mqtthost,
+                                           client_id=mqttclient)
+                        if register == "active_grid_powers":
+                            # Publish the voltage values as an array
+                            publish.single(mqtttopic, payload=value, qos=0, retain=False, hostname=mqtthost,
+                                           client_id=mqttclient)
+#                        if register == "active_grid_powerfactors":
+ #                           # Publish the voltage values as an array
+ #                           publish.single(mqtttopic, payload=value, qos=0, retain=False, hostname=mqtthost,
+ #                                          client_id=mqttclient)
+                        else:
+                            # Publish other values directly
+                            publish.single(mqtttopic, payload=value, qos=0, retain=False, hostname=mqtthost,
+                                           client_id=mqttclient)
+                    except:
+                        pass
+
+            # Publish the grid currents as an array
+            if array_currents_grid.size > 0:
+                mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/currents"
+                payload_json = json.dumps(array_currents_grid.tolist())  # Convert to JSON string
+                publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
+
+            if array_voltages_grid.size > 0:
+                mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/voltages"
+                payload_json = json.dumps(array_voltages_grid.tolist())  # Convert to JSON string
+                publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
+            if array_powers_grid.size > 0:
+                mqtttopic = f"openWB/set/counter/{read_counter_number()}/get/powers"
+                payload_json = json.dumps(array_powers_grid.tolist())  # Convert to JSON string
+                publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
+#            if array_powers_grid.size > 0:
+#                mqtttopic = "openWB/set/counter/16/get/power_factors"
+#                payload_json = json.dumps(array_powerfactors_grid.tolist())  # Convert to JSON string
+#                publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
+
+
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            await bridge.stop()
+            break
+
+    await bridge.stop()
+
+mqtthost = read_mqtt_host()
+counternumber = read_counter_number()
+batnumber = read_bat_number()
+pvnumber = read_pv_number()
+loop.run_until_complete(huaweiReadValues(topic_mapping))
---
+++ wlan_app.py
@@ -0,0 +1,309 @@
+import time
+from flask import Flask, render_template, request, redirect
+from wifi import Cell
+import configparser
+import os
+import subprocess
+import shutil
+import requests
+import zipfile
+
+app = Flask(__name__)
+
+# Lese die MQTT-Host-Adresse aus der config.ini-Datei
+config = configparser.ConfigParser()
+config.read('/home/pi/huawei_bridge_openwb/config.ini')
+mqtt_host = config.get('MQTT', 'host')
+pv_number = config.get('pvnumber', 'value')
+bat_number = config.get('batnumber', 'value')
+counter_number = config.get('counternumber', 'value')
+
+# Initialisiere das WLAN-Interface
+wifi_interface = 0
+
+# Initialisiere die Variable für den Verbindungsstatus
+connection_status = None
+
+
+# Lese den Versionshinweis aus der huaweimqtt_v1_0.py-Datei
+def get_firmware_version():
+    try:
+        with open('/home/pi/huawei_bridge_openwb/huaweimqtt_v1_0.py', 'r') as f:
+            first_line = f.readline()
+            version = first_line.strip().replace("# version ", "")
+            return version
+    except Exception as e:
+        print(f"Fehler beim Auslesen der Firmware-Version: {e}")
+        return "Unbekannt"
+
+
+
+@app.route('/update_firmware', methods=['POST'])
+def update_firmware():
+    try:
+        # Datei herunterladen
+        url = 'https://lebensraum-wohnraum.de/huawei_bridge_openwb/huaweimqtt_v1_0.zip'
+        response = requests.get(url)
+
+        # Überprüfen, ob der Download erfolgreich war
+        if response.status_code == 200:
+            # Datei speichern
+            with open('/tmp/huaweimqtt_v1_0.zip', 'wb') as f:
+                f.write(response.content)
+
+            # Zip-Datei entpacken
+            with zipfile.ZipFile('/tmp/huaweimqtt_v1_0.zip', 'r') as zip_ref:
+                zip_ref.extractall('/tmp/huaweimqtt_v1_0')
+
+            # Datei kopieren und ersetzen
+            shutil.copy('/tmp/huaweimqtt_v1_0/huaweimqtt_v1_0.py', '/home/pi/huawei_bridge_openwb/huaweimqtt_v1_0.py')
+            # Hier die index.html-Datei kopieren und ersetzen
+            shutil.copy('/tmp/huaweimqtt_v1_0/index.html', '/home/pi/templates/index.html')
+             # Hier die config.ini-Datei kopieren und ersetzen
+            shutil.copy('/tmp/huaweimqtt_v1_0/config.ini', '/home/pi/huawei_bridge_openwb/config.ini')
+             # Hier die config.ini-Datei kopieren und ersetzen
+            shutil.copy('/tmp/huaweimqtt_v1_0/wlan_app.py', '/home/pi/wlan_app.py')
+
+            # Berechtigungen zurücksetzen
+            os.chown('/home/pi/huawei_bridge_openwb/huaweimqtt_v1_0.py', os.getuid(), os.getgid())
+            os.chmod('/home/pi/huawei_bridge_openwb/huaweimqtt_v1_0.py', 0o755)
+            os.chown('/home/pi/wlan_app.py', os.getuid(), os.getgid())
+            os.chmod('/home/pi/wlan_app.py', 0o755)
+            # Firmware-Version auslesen
+
+            with open('/home/pi/huawei_bridge_openwb/huaweimqtt_v1_0.py', 'r') as f:
+                first_line = f.readline()
+                if first_line.startswith('# version'):
+                    version_info = first_line.strip().split('vom ')[1]
+                    # Speichere die Version im Dateisystem ab oder gib sie auf dem Webinterface aus
+                    # Hier kannst du den Code einfügen, um die Version auf dem Webinterface anzuzeigen
+
+
+        else:
+            return f"Fehler beim Aktualisieren der Firmware: Statuscode {response.status_code}"
+
+    except Exception as e:
+        return f"Fehler beim Aktualisieren der Firmware: {e}"
+
+
+@app.route('/restart', methods=['POST'])
+def restart():
+    try:
+        os.system("sudo reboot")
+        return "Der Raspberry Pi wird neu gestartet..."
+    except Exception as e:
+        return f"Fehler beim Neustart des Raspberry Pi: {e}"
+
+
+def get_wifi_connection_status():
+    try:
+        result = subprocess.run(['iwconfig', 'wlan0'], capture_output=True, text=True, check=True)
+        if "Not-Associated" in result.stdout:
+            return "Nicht verbunden"
+        else:
+            # Extrahiere den Namen des verbundenen WLAN-Netzwerks aus der Ausgabe
+            start_index = result.stdout.find('ESSID:') + 7
+            end_index = result.stdout.find('"', start_index)
+            connected_ssid = result.stdout[start_index:end_index]
+            return f"Verbunden mit {connected_ssid}"
+    except subprocess.CalledProcessError:
+        return "Fehler beim Überprüfen der Verbindung"
+
+def restart_wifi_adapter():
+    try:
+        # WLAN-Adapter ausschalten
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'down'], check=True)
+        # WLAN-Adapter einschalten
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'up'], check=True)
+        # Kurze Wartezeit, damit der Adapter sich neu verbinden kann
+        time.sleep(5)
+    except subprocess.CalledProcessError:
+        print("Fehler beim Neustart des WLAN-Adapters")
+
+def connect_to_wifi(ssid, password):
+    # Erstelle eine temporäre wpa_supplicant-Konfigurationsdatei
+    config = f'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n\nnetwork={{\n    ssid="{ssid}"\n    psk="{password}"\n}}\n'
+    subprocess.run(['wpa_passphrase', ssid, password], input=config, text=True, check=True)
+
+    # Überprüfe, ob die alte Datei bereits existiert
+    if os.path.exists('/etc/wpa_supplicant/wpa_supplicant.conf'):
+        # Benenne die alte Datei um, um ein Backup zu erstellen
+        shutil.move('/etc/wpa_supplicant/wpa_supplicant.conf', '/etc/wpa_supplicant/wpa_supplicant.conf.bak')
+
+    # Verschiebe die wpa_supplicant.conf-Datei
+    try:
+        shutil.move('/home/pi/huawei_bridge_openwb/wpa_supplicant.conf', '/etc/wpa_supplicant/')
+    except FileNotFoundError:
+        # Die Datei existiert nicht, erstelle sie mit dem Inhalt
+        with open('/home/pi/huawei_bridge_openwb/wpa_supplicant.conf', 'w') as f:
+            f.write(config)
+        # Verschiebe die wpa_supplicant.conf-Datei erneut
+        shutil.move('/home/pi/huawei_bridge_openwb/wpa_supplicant.conf', '/etc/wpa_supplicant/')
+
+    # Deaktiviere und aktiviere das WLAN-Interface
+    try:
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'down'], check=True)
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'up'], check=True)
+
+        # Kurze Wartezeit, damit der Adapter sich neu verbinden kann
+        time.sleep(5)
+    # Führe die zusätzlichen Befehle aus
+        subprocess.run(['sudo', 'killall', 'wpa_supplicant'], check=True)
+        subprocess.run(['sudo', 'wpa_supplicant', '-B', '-i', 'wlan0', '-c', '/etc/wpa_supplicant/wpa_supplicant.conf'], check=True)
+        subprocess.run(['sudo', 'dhclient', 'wlan0'], check=True)
+
+        # Kurze Wartezeit, damit der Adapter sich neu verbinden kann
+        time.sleep(10)
+
+        # Überprüfe den WLAN-Verbindungsstatus
+        result = subprocess.run(['iwconfig', 'wlan0'], capture_output=True, text=True, check=True)
+        if "Not-Associated" in result.stdout:
+            print("Verbindung fehlgeschlagen.")
+            return False
+        else:
+            # Extrahiere den Namen des verbundenen WLAN-Netzwerks aus der Ausgabe
+            start_index = result.stdout.find('ESSID:') + 7
+            end_index = result.stdout.find('"', start_index)
+            connected_ssid = result.stdout[start_index:end_index]
+            print(f"Verbunden mit {connected_ssid}.")
+            return True
+
+    except subprocess.CalledProcessError:
+        print("Fehler beim Verbinden.")
+        return False
+
+
+@app.route('/')
+def index():
+    networks = Cell.all('wlan0')  # 'wlan0' kann sich je nach deiner Raspberry Pi-Konfiguration ändern
+
+    # Überprüfe den aktuellen WLAN-Verbindungsstatus
+    global connection_status
+    connection_status = get_wifi_connection_status()
+
+    # Versionshinweis auslesen
+    firmware_version = get_firmware_version()
+
+    return render_template('index.html', networks=networks, mqtt_host=mqtt_host,
+    connection_status=connection_status, firmware_version=firmware_version, pv_number=pv_number,
+    bat_number=bat_number, counter_number=counter_number)
+
+@app.route('/connect', methods=['POST'])
+def connect():
+    global connection_status  # Greife auf die globale Variable zu
+
+    ssid = request.form['ssid']
+    password = request.form['password']
+
+    if connect_to_wifi(ssid, password):
+        # Verbindung erfolgreich hergestellt
+        connection_status = f"Verbunden mit {ssid}"
+    else:
+        # Verbindung fehlgeschlagen
+        connection_status = "Fehler beim Verbinden"
+
+    restart_wifi_adapter()
+
+    return redirect('/')
+
+@app.route('/save_config', methods=['POST'])
+def save_config():
+    try:
+        new_mqtt_host = request.form['mqtt_host']
+        new_pv_value = request.form['pv_number']
+        new_counter_value = request.form['counter_number']
+        new_bat_value = request.form['bat_number']
+
+        # Update the values in the config.ini file
+        config.set('MQTT', 'host', new_mqtt_host)
+        config.set('pvnumber', 'value', new_pv_value)
+        config.set('counternumber', 'value', new_counter_value)
+        config.set('batnumber', 'value', new_bat_value)
+
+        with open('/home/pi/huawei_bridge_openwb/config.ini', 'w') as configfile:
+            config.write(configfile)
+
+        # Read the updated values from the file
+        config.read('/home/pi/huawei_bridge_openwb/config.ini')
+        global mqtt_host, pv_number, counter_number, bat_number
+        mqtt_host = config.get('MQTT', 'host')
+        pv_number = config.get('pvnumber', 'value')
+        counter_number = config.get('counternumber', 'value')
+        bat_number = config.get('batnumber', 'value')
+
+        return redirect('/')
+
+    except Exception as e:
+        return f"Fehler beim Speichern der Konfiguration: {e}"
+
+
+"""
+@app.route('/update_mqtt_config', methods=['POST'])
+def update_mqtt_config():
+    new_mqtt_host = request.form['mqtt_host']
+
+    # Aktualisiere die MQTT-Host-Adresse in der config.ini-Datei
+    config.set('MQTT', 'host', new_mqtt_host)
+    with open('/home/pi/huawei_bridge_openwb/config.ini', 'w') as configfile:
+        config.write(configfile)
+
+    # Lese die aktualisierte MQTT-Host-Adresse erneut aus der Datei
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    global mqtt_host
+    mqtt_host = config.get('MQTT', 'host')
+
+    return redirect('/')
+
+@app.route('/update_pv_config', methods=['POST'])
+def update_pv_config():
+    new_pv_value = request.form['pv_number']
+
+    # Aktualisiere den Wert in der config.ini-Datei
+    config.set('pvnumber', 'value', new_pv_value)
+    with open('/home/pi/huawei_bridge_openwb/config.ini', 'w') as configfile:
+        config.write(configfile)
+
+    # Lese den aktualisierten Wert erneut aus der Datei
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    global pv_number  # Hier sollte es "pv_number" statt "pvnumber_value" sein
+    pv_number = config.get('pvnumber', 'value')
+
+    return redirect('/')
+
+@app.route('/update_counter_config', methods=['POST'])
+def update_counter_config():
+    new_counter_value = request.form['counter_number']
+
+    # Aktualisiere den Wert in der config.ini-Datei
+    config.set('counternumber', 'value', new_counter_value)
+    with open('/home/pi/huawei_bridge_openwb/config.ini', 'w') as configfile:
+        config.write(configfile)
+
+    # Lese den aktualisierten Wert erneut aus der Datei
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    global counter_number  # Hier sollte es "pv_number" statt "pvnumber_value" sein
+    counter_number = config.get('counternumber', 'value')
+
+    return redirect('/')
+
+@app.route('/update_bat_config', methods=['POST'])
+def update_bat_config():
+    new_bat_value = request.form['bat_number']
+
+    # Aktualisiere den Wert in der config.ini-Datei
+    config.set('batnumber', 'value', new_bat_value)
+    with open('/home/pi/huawei_bridge_openwb/config.ini', 'w') as configfile:
+        config.write(configfile)
+
+    # Lese den aktualisierten Wert erneut aus der Datei
+    config.read('/home/pi/huawei_bridge_openwb/config.ini')
+    global bat_number  # Hier sollte es "pv_number" statt "pvnumber_value" sein
+    bat_number = config.get('batnumber', 'value')
+
+    return redirect('/')
+
+"""
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
+    print("Die Anwendung wird gestartet...")
