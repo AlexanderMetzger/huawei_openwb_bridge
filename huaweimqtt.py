c29b128c8007e45c2c374778a63f9b7d06b0786a
AlexanderMetzger <60460634+AlexanderMetzger@users.noreply.github.com>
2025-10-15 11:32:04 +0200

    Update huaweimqtt.py
    Update auf die neuen Topics von OpenWB

--- huaweimqtt.py
+++ huaweimqtt.py
@@ -67,58 +67,58 @@ async def huaweiReadValues(bridge, topic_mapping, mqtthost, mqttclient, register
                     mqtttopic = None
 
                     if register == 'input_power':
-                            mqtttopic = f"openWB/set/pv/{pvnumber}/get/power"
+                            mqtttopic = f"openWB/set/mqtt/pv/{pvnumber}/get/power"
                             message = result[0] * -1
                           #  print(f"Eingangsleistung: {result[0]}")
 
                     elif register == 'daily_yield_energy':
-                            mqtttopic = f"openWB/set/pv/{pvnumber}/get/exported"
+                            mqtttopic = f"openWB/set/mqtt/pv/{pvnumber}/get/exported"
                             message = result[0] * 1000
                           #  print(f"Taegliche Ertragsenergie: {result[0]}")
 
                     elif register == "storage_state_of_capacity":
-                            mqtttopic = f"openWB/set/bat/{bat_number}/get/soc"
+                            mqtttopic = f"openWB/set/mqtt/bat/{bat_number}/get/soc"
                             message = result[0]
 
                     elif register == "storage_charge_discharge_power":
-                            mqtttopic = f"openWB/set/bat/{bat_number}/get/power"
+                            mqtttopic = f"openWB/set/mqtt/bat/{bat_number}/get/power"
                             message = result[0]
 
                     elif register == "storage_current_day_charge_capacity":
-                            mqtttopic = f"openWB/set/bat/{bat_number}/get/imported"
+                            mqtttopic = f"openWB/set/mqtt/bat/{bat_number}/get/imported"
                             message = result[0] * 1000
 
                     elif register == "storage_current_day_discharge_capacity":
-                            mqtttopic = f"openWB/set/bat/{bat_number}/get/exported"
+                            mqtttopic = f"openWB/set/mqtt/bat/{bat_number}/get/exported"
                             message = result[0] * 1000
 
                     elif register == "power_meter_active_power":
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/power"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/power"
                             message = result[0]  * -1
 
                     elif register == "grid_frequency":
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/frequency"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/frequency"
                             message = result[0]
 
                     elif register == "grid_accumulated_energy":
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/imported"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/imported"
                             message = result[0] * 1000
 
                     elif register == "grid_exported_energy":
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/exported"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/exported"
                             message = result[0] * 1000
                         # Handle other registers (in this case, the grid currents)
                         # Append numerical grid currents to the array
                     elif register in ['active_grid_A_current', 'active_grid_B_current', 'active_grid_C_current']:
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/currents"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/currents"
                             array_currents_grid = np.append(array_currents_grid, result[0] * -1)
 
                     elif register in ['grid_A_voltage', 'grid_B_voltage', 'grid_C_voltage']:
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/voltages"
+                            mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/voltages"
                             array_voltages_grid = np.append(array_voltages_grid, result[0])
 
                     elif register in ['active_grid_A_power', 'active_grid_B_power', 'active_grid_C_power']:
-                            mqtttopic = f"openWB/set/counter/{counter_number}/get/powers"
+                            mqtttopic = f"openWB/set/mqtt/ounter/{counter_number}/get/powers"
                             array_powers_grid = np.append(array_powers_grid, result[0] * -1)
 
                     if mqtttopic:
@@ -127,16 +127,16 @@ async def huaweiReadValues(bridge, topic_mapping, mqtthost, mqttclient, register
                         pass
 
             if array_currents_grid.size > 0:
-                mqtttopic = f"openWB/set/counter/{counter_number}/get/currents"
+                mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/currents"
                 payload_json = json.dumps(array_currents_grid.tolist())  # Convert to JSON string
                 publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
 
             if array_voltages_grid.size > 0:
-                mqtttopic = f"openWB/set/counter/{counter_number}/get/voltages"
+                mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/voltages"
                 payload_json = json.dumps(array_voltages_grid.tolist())  # Convert to JSON string
                 publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
             if array_powers_grid.size > 0:
-                mqtttopic = f"openWB/set/counter/{counter_number}/get/powers"
+                mqtttopic = f"openWB/set/mqtt/counter/{counter_number}/get/powers"
                 payload_json = json.dumps(array_powers_grid.tolist())  # Convert to JSON string
                 publish.single(mqtttopic, payload=payload_json, qos=0, retain=False, hostname=mqtthost, client_id=mqttclient)
 
@@ -170,16 +170,16 @@ bat_number = config.getint('batnumber', 'value')
 
 # Dictionary MQTT-Topic Mapping
 topic_mapping = {
-    'input_power': f"openWB/set/pv/{pvnumber}/get/power",
-    'daily_yield_energy': f"openWB/set/pv/{pvnumber}/get/exported",
-    'grid_accumulated_energy': f"openWB/set/counter/{counter_number}/get/imported",
-    'power_meter_active_power': f"openWB/set/counter/{counter_number}/get/power",
-    'grid_frequency': f"openWB/set/counter/{counter_number}/get/frequency",
-    'grid_exported_energy': f"openWB/set/counter/{counter_number}/get/exported",
-    'storage_charge_discharge_power': f"openWB/set/bat/{bat_number}/get/power",
-    'storage_current_day_charge_capacity': f"openWB/set/bat/{bat_number}/get/imported",
-    'storage_current_day_discharge_capacity': f"openWB/set/bat/{bat_number}/get/exported",
-    'storage_state_of_capacity': f"openWB/set/bat/{bat_number}/get/soc",
+    'input_power': f"openWB/set/mqtt/pv/{pvnumber}/get/power",
+    'daily_yield_energy': f"openWB/set/mqtt/pv/{pvnumber}/get/exported",
+    'grid_accumulated_energy': f"openWB/set/mqtt/counter/{counter_number}/get/imported",
+    'power_meter_active_power': f"openWB/set/mqtt/counter/{counter_number}/get/power",
+    'grid_frequency': f"openWB/set/mqtt/counter/{counter_number}/get/frequency",
+    'grid_exported_energy': f"openWB/set/mqtt/counter/{counter_number}/get/exported",
+    'storage_charge_discharge_power': f"openWB/set/mqtt/bat/{bat_number}/get/power",
+    'storage_current_day_charge_capacity': f"openWB/set/mqtt/bat/{bat_number}/get/imported",
+    'storage_current_day_discharge_capacity': f"openWB/set/mqtt/bat/{bat_number}/get/exported",
+    'storage_state_of_capacity': f"openWB/set/mqtt/bat/{bat_number}/get/soc",
 }
 
 # Registernames
